94cfda3119d87098cb75c2734accbe95932ae8d0
esl1129 <esl112999@gmail.com>
2021-11-19 16:00:35 +0900

    Update 2021.11.19

---
+++ Pages/13460.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+let n = readLine()!.components(separatedBy: " ").map{Int(String($0))!}
+var arr = [String]()
+
+for _ in 0..<n[0]{
+    arr.append(String(readLine()!))
+}
+// board, n == row, m == col
+func solution(_ board: [String], _ n: Int, _ m: Int){
+    
+}
+solution(arr,n[0],n[1])
