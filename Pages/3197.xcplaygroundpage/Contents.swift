195a0cdf0475cb929ce0389bad0ed33a5f59ac33
esl1129 <esl112999@gmail.com>
2021-12-07 16:27:20 +0900

    First Try Problem 3197 Update 2021.12.07

--- Pages/3197.xcplaygroundpage/Contents.swift
+++ Pages/3197.xcplaygroundpage/Contents.swift
@@ -1,56 +1,95 @@
 import Foundation
 
+struct Point: Hashable{
+    let x: Int
+    let y: Int
+    init(_ x: Int, _ y: Int){
+        self.x = x
+        self.y = y
+    }
+}
+
+func isBound(_ xx: Int, _ yy: Int) -> Bool{
+    return xx >= 0 && yy >= 0 && xx < ROW && yy < COL
+}
+
+func findWay(_ mid: Int) -> Bool{
+    var visited = [[Bool]](repeating: [Bool](repeating: false, count: COL), count: ROW)
+    let start = swan[0]
+    let end = swan[1]
+    visited[start.x][start.y] = true
+    var queue = [start]
+    while !queue.isEmpty{
+        let a = queue.removeFirst()
+        for k in 0..<4{
+            let xx = a.x + dx[k]
+            let yy = a.y + dy[k]
+            if !isBound(xx, yy) || visited[xx][yy] || priceBoard[xx][yy] > mid{ continue }
+            if xx == end.x && yy == end.y { return true}
+            queue.append(Point(xx, yy))
+            visited[xx][yy] = true
+        }
+    }
+    return false
+}
+
+let dx = [0,1,0,-1]
+let dy = [1,0,-1,0]
 
 let line = readLine()!.components(separatedBy: " ").map{Int(String($0))!}
-let a = line[0]
-let b = line[1]
+let ROW = line[0]
+let COL = line[1]
+
 var board = [[String]]()
-let dx = [-1,0,1,0]
-let dy = [0,-1,0,1]
-var start = [Int]()
-var end = [Int]()
-
-for i in 0..<a{
-    let line = readLine()!.map{String($0)}
-    if line.contains("L"){
-        if start.isEmpty{
-            start = [i,line.firstIndex(of: "L")!]
-        }else{
-            end = [i,line.firstIndex(of: "L")!]
-        }
-        board.append(line)
-    }
+var iceSet = [Point]()
+for _ in 0..<ROW{
+    board.append(readLine()!.map{String($0)})
 }
 
-
-func solution(_ board: [[String]],_ row: Int,_ col: Int, _ start: [Int], _ end: [Int]) -> Int{
-    var visited = [[Bool]](repeating: [Bool](repeating: false, count: col), count: row)
-    visited[start[0]][start[1]] = true
-    var queue = [[start[0],start[1],0]]
-    var min = Int.max
-    while !queue.isEmpty{
-        let point = queue.removeFirst()
-        for k in 0..<4{
-            let xx = point[0]+dx[k]
-            let yy = point[1]+dy[k]
-            if !isBound(xx,yy) || visited[xx][yy] { continue }
-            
-            var cnt = point[2]
-            if board[xx][yy] == "X" { cnt += 1}
-            else {
-                min = cnt < min && cnt != 0 ? cnt : min
-                cnt = 0
+var swan = [Point]()
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: COL), count: ROW)
+for i in 0..<ROW{
+    for j in 0..<COL{
+        if board[i][j] == "L" { swan.append(Point(i, j))}
+        if board[i][j] == "X" {
+            for k in 0..<4{
+                let xx = i+dx[k]
+                let yy = j+dy[k]
+                if !isBound(xx, yy) || iceSet.contains(Point(xx, yy)) || board[xx][yy] != "." { continue }
+                iceSet.append(Point(i, j))
+                visited[i][j] = true
+                break
             }
-            visited[xx][yy] = true
-            queue.append([xx,yy,cnt])
         }
     }
-    
-    return min
 }
 
-func isBound(_ x: Int, _ y: Int) -> Bool{
-    return x > 0 && y > 0 && x <= a && y <= b
+var priceBoard = [[Int]](repeating: [Int](repeating: 0, count: COL), count: ROW)
+var price = 1
+while !iceSet.isEmpty{
+    for _ in 0..<iceSet.count{
+        let a = iceSet.removeFirst()
+        priceBoard[a.x][a.y] = price
+        board[a.x][a.y] = "."
+        for k in 0..<4{
+            let xx = a.x+dx[k]
+            let yy = a.y+dy[k]
+            if !isBound(xx, yy) || board[xx][yy] != "X" || visited[xx][yy] { continue }
+            iceSet.append(Point(xx,yy))
+            visited[xx][yy] = true
+        }
+    }
+    price += 1
 }
 
-print(solution(board, a, b, start, end))
+var start = 0
+
+while start <= price{
+    let mid = (start+price)/2
+    if findWay(mid){
+        price = mid-1
+    }else{
+        start = mid+1
+    }
+}
+print(start)
