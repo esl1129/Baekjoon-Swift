e68e46549bacf7391be1dce091a8850bf94f1b30
esl1129 <esl112999@gmail.com>
2021-12-11 19:11:25 +0900

    Problem 17070 Update 2021.12.11

--- Pages/17070.xcplaygroundpage/Contents.swift
+++ Pages/17070.xcplaygroundpage/Contents.swift
@@ -6,80 +6,24 @@ func isBound(_ pp: Int) -> Bool{
     return pp >= 0 && pp < N
 }
 
-enum dir: Int{
-    case hor = 0, ver = 1, dia = 2
-}
-struct Pipe{
-    let startX: Int
-    let startY: Int
-    let endX: Int
-    let endY: Int
-    let dir: dir
-    func getHorizon() -> Pipe{
-        return Pipe(startX: endX, startY: endY, endX: endX, endY: endY+1, dir: .hor)
-    }
-    func getVertical() -> Pipe{
-        return Pipe(startX: endX, startY: endY, endX: endX+1, endY: endY, dir: .ver)
-    }
-    func getDiagnal() -> Pipe{
-        return Pipe(startX: endX, startY: endY, endX: endX+1, endY: endY+1, dir: .dia)
-    }
-}
 let N = Int(readLine()!)!
 var board = [[Int]]()
-
 for _ in 0..<N{
     board.append(readLine()!.components(separatedBy: " ").map{Int(String($0))!})
 }
-var answer = 0
-let start = Pipe(startX: 0, startY: 0, endX: 0, endY: 1, dir: .hor)
-var queue = [start]
-while !queue.isEmpty{
-    let a = queue.removeFirst()
-    if isBound(a.endY+1) && isBound(a.endX+1) && board[a.endX][a.endY+1] != 1 && board[a.endX+1][a.endY] != 1 && board[a.endX+1][a.endY+1] != 1{
-        let pipe = a.getDiagnal()
-        if pipe.endX == N-1 && pipe.endY == N-1 {
-            answer += 1
-        }else{
-            queue.append(pipe)
+var sumBoard = [[[Int]]](repeating: [[Int]](repeating: [0,0,0], count: N), count: N)
+sumBoard[0][1][0] = 1
+for i in 0..<N{
+    for j in 0..<N{
+        if isBound(i-1) && isBound(j-1) && board[i][j-1] == 0 && board[i-1][j] == 0 && board[i][j] == 0{
+            sumBoard[i][j][2] += sumBoard[i-1][j-1].reduce(0, +)
         }
-    }
-    switch a.dir{
-    case .hor:
-        if isBound(a.endY+1) && board[a.endX][a.endY+1] != 1 {
-            let pipe = a.getHorizon()
-            if pipe.endX == N-1 && pipe.endY == N-1 {
-                answer += 1
-                break
-            }
-            queue.append(pipe)
+        if isBound(i-1) && board[i-1][j] == 0 && board[i][j] == 0{
+            sumBoard[i][j][1] += sumBoard[i-1][j][1]+sumBoard[i-1][j][2]
         }
-    case .ver:
-        if isBound(a.endX+1) && board[a.endX+1][a.endY] != 1 {
-            let pipe = a.getVertical()
-            if pipe.endX == N-1 && pipe.endY == N-1 {
-                answer += 1
-                break
-            }
-            queue.append(pipe)
-        }
-    case .dia:
-        if isBound(a.endY+1) && board[a.endX][a.endY+1] != 1 {
-            let pipe = a.getHorizon()
-            if pipe.endX == N-1 && pipe.endY == N-1 {
-                answer += 1
-            }else{
-                queue.append(pipe)
-            }
-        }
-        if isBound(a.endX+1) && board[a.endX+1][a.endY] != 1 {
-            let pipe = a.getVertical()
-            if pipe.endX == N-1 && pipe.endY == N-1 {
-                answer += 1
-            }else{
-                queue.append(pipe)
-            }
+        if isBound(j-1) && board[i][j-1] == 0 && board[i][j] == 0{
+            sumBoard[i][j][0] += sumBoard[i][j-1][0]+sumBoard[i][j-1][2]
         }
     }
 }
-print(answer)
+print(sumBoard[N-1][N-1].reduce(0, +))
